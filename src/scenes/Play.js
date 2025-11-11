291de7116e684d40c4e76b70354eaf456592bf01
esdee303 <steven.deferme@gmail.com>
2025-11-11 13:01:54 +0100

    S04E117 Platform as the layer collider

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -9,7 +9,8 @@ class PlayScene extends Phaser.Scene {
     const map = this.createMap();
     const layers = this.createLayers(map);
 
-    this.createPlayer();
+    const player = this.createPlayer();
+    this.physics.add.collider(player, layers.platforms);
   }
 
   createMap() {
@@ -21,7 +22,9 @@ class PlayScene extends Phaser.Scene {
   createLayers(map) {
     const tileset = map.getTileset('main_lev_build_1');
     const environment = map.createStaticLayer('environment', tileset);
-    const platforms = map.createDynamicLayer('platforms', tileset);
+    const platforms = map.createStaticLayer('platforms', tileset);
+
+    platforms.setCollisionByExclusion(-1, true);
     return { environment, platforms };
   }
 
@@ -29,6 +32,7 @@ class PlayScene extends Phaser.Scene {
     const player = this.physics.add.sprite(100, 250, 'player');
     player.body.setGravityY(500);
     player.setCollideWorldBounds(true);
+    return player;
   }
 }
 
