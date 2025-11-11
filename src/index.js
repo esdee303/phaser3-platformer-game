048922f2aa0635d5c725f322ea215c1048b107c2
esdee303 <steven.deferme@gmail.com>
2025-11-11 21:12:53 +0100

    Map changes

--- src/index.js
+++ src/index.js
@@ -6,12 +6,13 @@ const MAP_WIDTH = 1600;
 
 const WIDTH = document.body.offsetWidth;
 const HEIGHT = 600;
+const ZOOM_FACTOR = 1.2
 
 const SHARED_CONFIG = {
   mapOffset: MAP_WIDTH > WIDTH ? MAP_WIDTH - WIDTH : 0,
   width: WIDTH,
   height: HEIGHT,
-  zoomFactor: 1.5,
+  zoomFactor: ZOOM_FACTOR,
 };
 
 const Scenes = [PreloadScene, PlayScene];
